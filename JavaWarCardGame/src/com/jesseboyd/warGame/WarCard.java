ecb212b232d91a2f36ce85fbca91ab4e7e1b2c9d
Jesse Boyd <jesseboydconsulting@gmail.com>
2016-09-30 13:20:49 -0700

    another update

--- JavaWarCardGame/src/com/jesseboyd/warGame/WarCard.java
+++ JavaWarCardGame/src/com/jesseboyd/warGame/WarCard.java
@@ -2,19 +2,15 @@ package com.jesseboyd.warGame;
 
 public class WarCard implements Card {
 
-	// war cards can have a value from 1-14
 	private int cardValue;
 	
 	public WarCard(int cardValue){
-		if(cardValue < 1 || cardValue > 16 ){
-			throw new IllegalArgumentException("Card Value Outside of range");
-		}
+//		if(cardValue < 1 || cardValue > 16 ){
+//			throw new IllegalArgumentException("Card Value Outside of range");
+//		}
 		this.cardValue = cardValue;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.jesseboyd.warGame.Card#getValue()
-	 */
 	public int getValue() {
 		return cardValue;
 	}
